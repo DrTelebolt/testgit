d909f0f2a3d2928399f55922705601a011626ceb
DrTelebolt <dr.telebol@gmail.com>
2026-02-26 11:51:36 +0300

    master9

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ txt = "i love the world"
 print(txt,"!")
 for i in range(10):
     txt+=" very"
-    print(txt,"much!")
+    print(txt,"much","!")
 
 z=input("введи z:")
-print("z=",z**2)
+print("z=",int(z)**2)
 
 print("i like 2 moved moved")
 
